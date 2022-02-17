177f82c8b0b1b3662669eeb81ef958e398f521d7
Flawws <55849405+FlawwsX@users.noreply.github.com>
2022-02-17 12:10:38 +0000

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,25 +1,15 @@
-local function toggleNuiFrame(shouldShow)
-  SetNuiFocus(shouldShow, shouldShow)
-  SendReactMessage('setVisible', shouldShow)
-end
+local TEMPLATE = {
+  
+  isOpen = false,
+  
+  ToggleNui = function(self, open)
+    self.isOpen = open
+    SetNuiFocus(open, open)
+    SendReactMessage('setVisible', open)
+  end,
+  
+}
 
-RegisterCommand('show-nui', function()
-  toggleNuiFrame(true)
-  debugPrint('Show NUI frame')
-end)
-
-RegisterNUICallback('hideFrame', function(_, cb)
-  toggleNuiFrame(false)
-  debugPrint('Hide NUI frame')
-  cb({})
-end)
-
-RegisterNUICallback('getClientData', function(data, cb)
-  debugPrint('Data sent by React', json.encode(data))
-
--- Lets send back client coords to the React frame for use
-  local curCoords = GetEntityCoords(PlayerPedId())
-
-  local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
-  cb(retData)
-end)
+--[[exports("exampleExport", function(...)
+  return TEMPLATE:ToggleNui(...)
+end)]]
