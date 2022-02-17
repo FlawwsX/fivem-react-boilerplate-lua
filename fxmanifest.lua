6fc3e0ce2558ef72cc32e96f102c5ebebaf5a201
Flawws <55849405+FlawwsX@users.noreply.github.com>
2022-02-17 12:08:58 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,12 @@
 fx_version "cerulean"
-
-description "Basic React (TypeScript) & Lua Game Scripts Boilerplate"
-author "Project Error"
-version '1.0.0'
-repository 'https://github.com/project-error/fivem-react-boilerplate-lua'
-
 lua54 'yes'
 
-games {
-  "gta5",
-  "rdr3"
-}
+games { "gta5", "rdr3" }
 
 ui_page 'web/build/index.html'
 
-client_script "client/**/*"
 server_script "server/**/*"
+client_script "client/**/*"
 
 files {
   'web/build/index.html',
